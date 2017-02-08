7131d2bef70555871bbafbbb34d9ba9cd0fdec37
thomas <tjohander@gmail.com>
2017-02-08 00:08:47 -0600

    Comments to the JS added

--- client.js
+++ client.js
@@ -5,19 +5,24 @@ Your function should return a string that combines all of the values starting at
 
 */
 
-
+//Here is my array
 var myArray = ["USDA","Prime","Academy","Is","Where","I","Want","To","Brew","Java","Script!"];
 
+//here is where I declare the function
 function concatArrayIndices(array, number){
 
+//In order to get only the array items I want, I will create an empty array to hold the items I want
   var tempArray = [];
 
+//We will then loop through each item in the array, starting with the first index number I want to include.  We will loop through each of the array elements and execute a small block of code
   for (var i = number; i < array.length; i++){
 
+//the codeblock that will execute simply pushes each item into the temporary array of 'keepables'
     tempArray.push(array[i]);
 
   }
 
+//I create a new string to hold the concatenated values of the tempArray, joined with an empty space.  I will simply return the newString string.
 var newString = tempArray.join(' ');
 return newString;
 
@@ -25,11 +30,3 @@ return newString;
 }
 
 console.log(concatArrayIndices(myArray,1));
-
-
-
-
-
-
-
-///
